980bcb223e1ff07a77d32e722aea0ffbc3105217
williamstyronejr <itenory@gmail.com>
2021-12-03 01:45:48 -0500

    Add auth router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,29 @@
 import { Application, Request, Response, NextFunction } from 'express';
+import AuthRouter from './auth';
 import logger from '../services/logger';
 
 export default function setupRoutes(app: Application) {
-  // Error handler
+  app.use(AuthRouter);
+
+  // Catch all error handler
   app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     if (err) {
       logger.error(err);
+
+      if (err.status && err.status === 400) {
+        switch (err.status) {
+          case 400: // Invalid user input
+            res.status(err.status).json({ errors: err.msg });
+            break;
+          default:
+            res
+              .status(500)
+              .send('An error occurred on the server, please try again.');
+        }
+        return;
+      }
     }
 
-    res.status(500).send('Server error occurred, please try again.');
+    return res.status(500).send('Server error occurred, please try again.');
   });
 }
