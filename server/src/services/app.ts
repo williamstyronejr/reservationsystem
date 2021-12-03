43916836b378acb6caae4b392c15fb861244e622
williamstyronejr <itenory@gmail.com>
2021-12-03 01:46:24 -0500

    Passport setup

--- server/src/services/app.ts
+++ server/src/services/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import passport from 'passport';
 import setupRoutes from '../routes/index';
 
+require('./passport');
+
 const app = express();
+
+app.use(passport.initialize());
 
 setupRoutes(app);
 
