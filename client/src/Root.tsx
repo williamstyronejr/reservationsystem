5097142dc70359b1d6cd906fcbbfe48f4bbbe1e3
williamstyronejr <itenory@gmail.com>
2021-12-10 22:36:07 -0500

    Setup react query

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { AuthProvider } from './context/auth';
 import HomePage from './pages/Homepage';
+import RecoveryPage from './pages/auth/RecoveryPage';
+
+const queryClient = new QueryClient();
 
 const Root = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recovery" element={<RecoveryPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </AuthProvider>
 );
 
 export default Root;
