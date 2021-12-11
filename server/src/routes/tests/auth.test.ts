19f25968ee8e132b70ad6ae0e2239c5f29bd8888
williamstyronejr <itenory@gmail.com>
2021-12-10 22:34:45 -0500

    Created test for auth routes

--- server/src/routes/tests/auth.test.ts
+++ server/src/routes/tests/auth.test.ts
@@ -15,8 +15,10 @@ afterAll(async () => {
 });
 
 describe('User signup', () => {
+  const routeToTest = () => '/signup';
+
   test('Empty parameter should throw 400 error with error message', async () => {
-    const res = await request(server).post('/signup').expect(400);
+    const res = await request(server).post(routeToTest()).expect(400);
 
     expect(res.body.errors.username).toBeDefined();
     expect(res.body.errors.email).toBeDefined();
@@ -31,7 +33,7 @@ describe('User signup', () => {
     const confirm = 'co';
 
     const res = await request(server)
-      .post('/signup')
+      .post(routeToTest())
       .send({ username, password, email, confirm })
       .expect(400);
 
@@ -48,8 +50,44 @@ describe('User signup', () => {
     const confirm = 'password';
 
     await request(server)
+      .post(routeToTest())
+      .send({ username, password, email, confirm })
+      .expect(200);
+  });
+});
+
+describe('User signin', () => {
+  const routeToTest = () => '/signin';
+  const username = createRandomString();
+  const email = createRandomString(8, '@email.com');
+  const password = 'password';
+  const confirm = 'password';
+
+  beforeAll(async () => {
+    // Create a user for testing
+    await request(server)
       .post('/signup')
       .send({ username, password, email, confirm })
+      .expect(200);
+  });
+
+  test('Empty parameter should throw 401 error', async () => {
+    await request(server).post(routeToTest()).send().expect(400);
+  });
+
+  test('Incorrect parameters should throw 401 error', async () => {
+    const incorrectUsername = createRandomString();
+
+    await request(server)
+      .post(routeToTest())
+      .send({ username: incorrectUsername, password })
+      .expect(401);
+  });
+
+  test('Correct signin should response 200', async () => {
+    await request(server)
+      .post(routeToTest())
+      .send({ username, password })
       .expect(200);
   });
 });
