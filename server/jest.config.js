c5ef897d22b981a0ed562f9f2ee8f272f24e4b5c
williamstyronejr <itenory@gmail.com>
2021-12-10 22:43:36 -0500

    No longer runs test in build

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>src/setupTest.js'],
+  setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
+  rootDir: 'src',
 };
