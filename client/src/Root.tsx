800ec0ad026436d51f76dd40605ce63ef13ad0b2
TyroneWilliams <itenory@gmail.com>
2021-12-11 22:04:31 -0500

    Add signin/signup routes

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { AuthProvider } from './context/auth';
 import HomePage from './pages/Homepage';
+import SigninPage from './pages/auth/SigninPage';
+import SignupPage from './pages/auth/SignupPage';
 import RecoveryPage from './pages/auth/RecoveryPage';
 
 const queryClient = new QueryClient();
@@ -14,6 +16,8 @@ const Root = () => (
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recovery" element={<RecoveryPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
