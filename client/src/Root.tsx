d1b3af186b0d7e19f917cee5317f87dfbecfe7c4
williamstyronejr <itenory@gmail.com>
2021-12-18 12:32:57 -0500

    Updates settings for react query

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -6,22 +6,36 @@ import HomePage from './pages/Homepage';
 import SigninPage from './pages/auth/SigninPage';
 import SignupPage from './pages/auth/SignupPage';
 import RecoveryPage from './pages/auth/RecoveryPage';
+import Header from './components/Header';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 846000,
+    },
+  },
+});
 
 const Root = () => (
-  <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/recovery" element={<RecoveryPage />} />
-          <Route path="/signin" element={<SigninPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <main className="page__main">
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/recovery" element={<RecoveryPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </AuthProvider>
+  </QueryClientProvider>
 );
 
 export default Root;
