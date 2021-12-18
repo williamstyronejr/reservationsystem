30f4bc747e2f22045fe381e4db779988ecb99098
williamstyronejr <itenory@gmail.com>
2021-12-18 12:32:17 -0500

    Accepts error prop

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 
-const Input = ({ name, type }: { name: string; type: string }) => {
+const Input = ({
+  name,
+  type,
+  error,
+}: {
+  name: string;
+  type: string;
+  error: string | null;
+}) => {
   const [value, setValue] = React.useState<string>('');
 
   return (
-    <label htmlFor={`${name}-input`}>
+    <label className="form__label" htmlFor={`${name}-input`}>
+      {error ? <div className="form__fielderror">{error}</div> : null}
       <input
         id={`${name}-input`}
         name={name}
+        className="form__input form__input--text"
         type={type}
         onChange={(evt) => {
           setValue(evt.target.value);
