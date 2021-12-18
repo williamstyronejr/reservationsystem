2a195a04b0054f5f871fdcd8a1118fd2986750b2
williamstyronejr <itenory@gmail.com>
2021-12-18 12:34:02 -0500

    Updated for cookie and csrftoken

--- server/src/routes/tests/auth.test.ts
+++ server/src/routes/tests/auth.test.ts
@@ -1,8 +1,11 @@
 import request from 'supertest';
 import startServer from '../../server';
 import { createRandomString } from '../../utils/utils';
+import getSession from './utils';
 
 let server: any;
+let csrfToken: string;
+let cookie: string;
 
 const { IP, PORT } = process.env;
 
@@ -17,41 +20,53 @@ afterAll(async () => {
 describe('User signup', () => {
   const routeToTest = () => '/signup';
 
+  beforeAll(async () => {
+    [csrfToken, cookie] = await getSession(request, server);
+  });
+
   test('Empty parameter should throw 400 error with error message', async () => {
-    const res = await request(server).post(routeToTest()).expect(400);
+    const res = await request(server)
+      .post(routeToTest())
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
+      .expect(400);
 
     expect(res.body.errors.username).toBeDefined();
     expect(res.body.errors.email).toBeDefined();
     expect(res.body.errors.password).toBeDefined();
-    expect(res.body.errors.confirm).toBeDefined();
+    expect(res.body.errors.confirmPassword).toBeDefined();
   });
 
   test('Invalid parameters should throw 400 error with error message', async () => {
     const username = 'u';
     const password = 'p';
     const email = ' email';
-    const confirm = 'co';
+    const confirmPassword = 'co';
 
     const res = await request(server)
       .post(routeToTest())
-      .send({ username, password, email, confirm })
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
+      .send({ username, password, email, confirmPassword })
       .expect(400);
 
     expect(res.body.errors.username).toBeDefined();
     expect(res.body.errors.email).toBeDefined();
     expect(res.body.errors.password).toBeDefined();
-    expect(res.body.errors.confirm).toBeDefined();
+    expect(res.body.errors.confirmPassword).toBeDefined();
   });
 
   test('Valid parameters should response 200 ', async () => {
     const username = createRandomString();
     const email = createRandomString(8, '@email.com');
     const password = 'password';
-    const confirm = 'password';
+    const confirmPassword = 'password';
 
     await request(server)
       .post(routeToTest())
-      .send({ username, password, email, confirm })
+      .send({ username, password, email, confirmPassword })
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
       .expect(200);
   });
 });
@@ -61,18 +76,27 @@ describe('User signin', () => {
   const username = createRandomString();
   const email = createRandomString(8, '@email.com');
   const password = 'password';
-  const confirm = 'password';
+  const confirmPassword = 'password';
 
   beforeAll(async () => {
+    [csrfToken, cookie] = await getSession(request, server);
+
     // Create a user for testing
     await request(server)
       .post('/signup')
-      .send({ username, password, email, confirm })
+      .send({ username, password, email, confirmPassword })
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
       .expect(200);
   });
 
   test('Empty parameter should throw 401 error', async () => {
-    await request(server).post(routeToTest()).send().expect(400);
+    await request(server)
+      .post(routeToTest())
+      .send()
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
+      .expect(400);
   });
 
   test('Incorrect parameters should throw 401 error', async () => {
@@ -81,6 +105,8 @@ describe('User signin', () => {
     await request(server)
       .post(routeToTest())
       .send({ username: incorrectUsername, password })
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
       .expect(401);
   });
 
@@ -88,6 +114,8 @@ describe('User signin', () => {
     await request(server)
       .post(routeToTest())
       .send({ username, password })
+      .set('Cookie', cookie)
+      .set('csrf-token', csrfToken)
       .expect(200);
   });
 });
