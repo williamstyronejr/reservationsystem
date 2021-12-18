d8c8713ef2a95ae6ce02916491bdaec55e5f04fb
williamstyronejr <itenory@gmail.com>
2021-12-18 12:43:52 -0500

    Updates serialize for passport

--- server/src/services/passport.ts
+++ server/src/services/passport.ts
@@ -1,12 +1,23 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import db from '../models/index';
+import { findUserById } from './user';
 import { compareHash } from '../utils/utils';
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user: any, done) => done(null, user));
+passport.serializeUser((user: any, done) => done(null, { id: user.id }));
+passport.deserializeUser((user: any, done) => {
+  findUserById(user.id)
+    .then((res: any) => {
+      if (!res) return done(null, false);
+      done(null, res.dataValues);
+    })
+    .catch(() => {
+      done(null, false);
+    });
+});
 
 const localStrategy = new LocalStrategy(
+  { session: true },
   (username: string, password: string, done) => {
     db.models.User.findOne({ where: { username } })
       .then((user: any) => {
