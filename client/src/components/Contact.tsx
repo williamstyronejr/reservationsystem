87702c65184a7b381a26baf269ea5705b1173772
williamstyronejr <itenory@gmail.com>
2021-12-22 22:18:07 -0500

    Now uses textarea for message

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
         <Input name="name" type="text" label="Your Name" error={null} />
         <Input name="email" type="text" label="Your Email" error={null} />
         <Input name="subject" type="text" label="Your Subject" error={null} />
-        <Input name="message" type="text" label="Message" error={null} />
+        <Input name="message" type="textarea" label="Message" error={null} />
       </fieldset>
 
       <button className="form__btn form__btn--submit" type="submit">
